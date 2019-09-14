41a51fea45338127991728bc6fe5d69bfa1ff00d
M-garrigan <mgarrigan99@gmail.com>
2019-09-14 16:33:15 -0400

    Making the slide deck an array of components that can be iterated over

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import SL99 from './slide/sl99.js';
+import S_basics from './slide/s-basics.js';
+import S_basic_flow from './slide/s-basic-flow';
+import S_basic_code from './slide/s-basic-code';
+
+
 export default props => {
+  const slideArray = [
+    {comp: <SL99 height={props.height} width={props.width}/>},
+    {comp: <S_basics />},
+    {comp: <S_basic_flow />},
+    {comp: <S_basic_code />}
+  ];
+  
 
   return (
     <div className="slide-wrapper">
-      
+      {slideArray[props.currentSlide].comp}
     </div>
   );
 };
