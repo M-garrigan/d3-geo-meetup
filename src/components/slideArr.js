64ac1599a25b094c9c8c21ac33b4d25f69218b1c
M-garrigan <mgarrigan99@gmail.com>
2019-09-26 14:44:37 -0400

    An array of every slide to be shown

---
+++ src/components/slideArr.js
@@ -0,0 +1,49 @@
+
+import Zero from './slide/zero.js';
+import Basics from './slide/basics.js';
+import BasicFlow from './slide/basic-flow';
+import BasicFlowAlt from './slide/basic-flow-alt';
+import BasicCode from './slide/basic-code';
+import GeojsonOverview from './slide/geojson-overview.js';
+import Antimeridian from './slide/antimeridian.js';
+import GeojsonGeometryObject from './slide/geojson-geometry-obj.js';
+import GeojsonZeroDim from './slide/geojson-zero-dim.js';
+import GeojsonOneDim from './slide/geojson-one-dim.js';
+import GeojsonTwoDim from './slide/geojson-two-dim.js';
+import GeojsonHetero from './slide/geojson-hetero.js';
+import TopoJSON from './slide/topojson.js';
+import ShapeFiles from './slide/shapefiles.js';
+import ConvertingData from './slide/converting-data.js';
+import DataSources from './slide/data-sources.js';
+import Projection from './slide/projection.js';
+import ProjectionMeth from './slide/projection-meth.js';
+import ProjectionRotate from './slide/projection-rotate.js';
+import CanvasSVG from './slide/canvas-svg.js';
+import Path from './slide/path.js';
+import PathMeth from './slide/path-meth';
+
+
+export default [
+  Zero,
+  Basics,
+  BasicFlow,
+  BasicFlowAlt,
+  BasicCode,
+  GeojsonOverview,
+  Antimeridian,
+  GeojsonGeometryObject,
+  GeojsonZeroDim,
+  GeojsonOneDim,
+  GeojsonTwoDim,
+  GeojsonHetero,
+  TopoJSON,
+  ShapeFiles,
+  ConvertingData,
+  DataSources,
+  Projection,
+  ProjectionMeth,
+  ProjectionRotate,
+  CanvasSVG,
+  Path,
+  PathMeth,
+];
