0a8e9d7bbfd8c12fa7589c5d33d65303ac417f4e
M-garrigan <mgarrigan99@gmail.com>
2019-11-14 11:24:30 -0500

    Disabling button based on the first and last slide shown

--- src/components/BottomControl.js
+++ src/components/BottomControl.js
@@ -19,21 +19,41 @@ export default ({height, width, currentSlide, setCurrentSlide}) => {
   return (
     <div className="bottomcontrol-wrapper">
 
-      <div 
-          className="bottomcontrol-button" 
-          onClick={() => setCurrentSlide(currentSlide => currentSlide - 1)}
-        >
-          <svg
-            className="svg-angles-button"
-            height={height90}
-            viewBox="0 0 448 512"
-            width={height90 * 1.33}
-          >
-            <path
-              d="M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"
-            />
-          </svg>
-        </div>
+      {
+        currentSlide === 0
+          ? (
+            <div 
+              className="bottomcontrol-button-disabled" 
+            >
+              <svg
+                className="svg-angles-button"
+                height={height90}
+                viewBox="0 0 448 512"
+                width={height90 * 1.33}
+              >
+                <path
+                  d="M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"
+                />
+              </svg>
+            </div>
+          ) : (
+            <div 
+              className="bottomcontrol-button" 
+              onClick={() => setCurrentSlide(currentSlide => currentSlide - 1)}
+            >
+              <svg
+                className="svg-angles-button"
+                height={height90}
+                viewBox="0 0 448 512"
+                width={height90 * 1.33}
+              >
+                <path
+                  d="M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"
+                />
+              </svg>
+            </div>
+          )
+      }
 
         <div className="bottomcontrol-thumbnail-wrapper">
 
@@ -124,21 +144,41 @@ export default ({height, width, currentSlide, setCurrentSlide}) => {
           
         </div>
 
-        <div 
-          className="bottomcontrol-button" 
-          onClick={() => setCurrentSlide(currentSlide => currentSlide + 1)}
-        >
-          <svg
-            className="svg-angles-button"
-            height={height90}
-            viewBox="0 0 448 512"
-            width={height90 * 1.33}
-          >
-            <path
-              d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"
-            />
-          </svg>
-        </div>
+        {
+          currentSlide === slideArr.length - 1 
+            ? ( // show disabled button on last slide
+              <div 
+                className="bottomcontrol-button-disabled" 
+              >
+                <svg
+                  className="svg-angles-button"
+                  height={height90}
+                  viewBox="0 0 448 512"
+                  width={height90 * 1.33}
+                >
+                  <path
+                    d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div 
+                className="bottomcontrol-button" 
+                onClick={() => setCurrentSlide(currentSlide => currentSlide + 1)}
+              >
+                <svg
+                  className="svg-angles-button"
+                  height={height90}
+                  viewBox="0 0 448 512"
+                  width={height90 * 1.33}
+                >
+                  <path
+                    d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"
+                  />
+                </svg>
+              </div>
+            )
+        }
     </div>
   );
 };
